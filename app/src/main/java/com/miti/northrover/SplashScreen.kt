fe9d53553deb5ad2682df9ab0678e3f8b1cd318c
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-23 22:13:44 +0500

    SplashScreen.kt

--- app/src/main/java/com/miti/northrover/SplashScreen.kt
+++ app/src/main/java/com/miti/northrover/SplashScreen.kt
@@ -3,7 +3,7 @@ package com.miti.northrover
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
