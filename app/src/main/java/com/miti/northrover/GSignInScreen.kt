c2dfafbd7cd63b32a3bd584fb6e598328b381f29
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-24 14:33:19 +0500

    changes

--- app/src/main/java/com/miti/northrover/GSignInScreen.kt
+++ app/src/main/java/com/miti/northrover/GSignInScreen.kt
@@ -1,27 +1,57 @@
 package com.miti.northrover
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.miti.northrover.databinding.ActivityGsigninscreenBinding
 
 
 class GSignInScreen : AppCompatActivity() {
 
     lateinit var binding: ActivityGsigninscreenBinding
-
-    private val mGoogleSignInClient: GoogleSignInClient? = null
+    private var mGoogleSignInClient: GoogleSignInClient? = null
     private val RC_SIGN_IN = 123
-    private val mAuth: FirebaseAuth? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGsigninscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+
+        createRequest()
+
+        binding.cardGoogleSignIn.setOnClickListener {
+            Toast.makeText(this@GSignInScreen, "Clicked", Toast.LENGTH_SHORT)
+                .show()
+            signIn()
+        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun createRequest() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     override fun onStart() {
@@ -34,5 +64,37 @@ class GSignInScreen : AppCompatActivity() {
             val intent = Intent(this@GSignInScreen, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    val user = mAuth!!.currentUser
+                    val intent = Intent(this@GSignInScreen, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@GSignInScreen, "Sorry auth failed!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 }
