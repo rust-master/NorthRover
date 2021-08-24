44c76ed5020728070fdb910c30a5348874e8e908
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-24 14:03:21 +0500

    view binding

--- app/src/main/java/com/miti/northrover/MainActivity.kt
+++ app/src/main/java/com/miti/northrover/MainActivity.kt
@@ -2,10 +2,16 @@ package com.miti.northrover
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.miti.northrover.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
