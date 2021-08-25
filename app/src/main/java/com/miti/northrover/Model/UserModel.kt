ccfc664fc4f00eaa374e5124faa79aef533840c1
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-25 09:44:28 +0500

    Model code write

--- app/src/main/java/com/miti/northrover/Model/UserModel.kt
+++ app/src/main/java/com/miti/northrover/Model/UserModel.kt
@@ -1,4 +1,17 @@
 package com.miti.northrover.Model
 
 class UserModel {
+    var name: String? = null
+    var email: String? = null
+    var phone: String? = null
+    var address: String? = null
+
+    constructor()
+
+    constructor(name: String?, email: String?, phone: String?, address: String?) {
+        this.name = name
+        this.email = email
+        this.phone = phone
+        this.address = address
+    }
 }
